dba8c85856a81862d07114a231a271b36b55086b
northcu2 <northcu2@illinois.edu>
2015-03-05 20:02:10 -0600

    Attempt 2

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var cool = require('cool-ascii-faces');
 var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000))
+app.get('/', function(request, response) {
+ var result = ''
+ var times = process.env.TIMES || 5
+  for (i=0; i < times; i++)
+   result += cool();
+  response.send(result);
+});
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
@@ -16,13 +23,7 @@ app.get('/db', function (request, response) {
     });
   });
 })
-app.get('/', function(request, response) {
- var result = ''
- var times = process.env.TIMES || 5
-  for (i=0; i < times; i++)
-   result += cool();
-  response.send(result);
-});
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
